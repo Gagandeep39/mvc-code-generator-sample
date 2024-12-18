ebf5a283086c5e2720b1f5de719def6f8edb6078
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2024-12-18 07:01:50 -0500

    Added java.sql in module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.assetuijavafx {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires java.sql;
 
 
     opens org.example.assetuijavafx.models to javafx.base;
