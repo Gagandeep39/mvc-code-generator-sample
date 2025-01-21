58f9acfdcb3d861735d0eab4d96cb68ae2b938b4
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2025-01-21 18:35:56 -0500

    Updated UI dimensions

--- src/main/java/org/example/assetuijavafx/AssetPlusApplication.java
+++ src/main/java/org/example/assetuijavafx/AssetPlusApplication.java
@@ -25,7 +25,7 @@ public class AssetPlusApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(AssetPlusApplication.class.getResource("AssetPlus.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 960, 540);
         stage.setTitle("AssetPlus");
         stage.setScene(scene);
         stage.show();
