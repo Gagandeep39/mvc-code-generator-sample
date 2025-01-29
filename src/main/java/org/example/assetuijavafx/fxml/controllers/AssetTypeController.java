99d7a2e3ea30ec3d25972f35e0e12d01dc9366c5
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2025-01-29 09:09:34 -0500

    Implemented base controller

--- src/main/java/org/example/assetuijavafx/fxml/controllers/AssetTypeController.java
+++ src/main/java/org/example/assetuijavafx/fxml/controllers/AssetTypeController.java
@@ -10,25 +10,41 @@ import org.example.assetuijavafx.fxml.utils.PageSwitchEvent;
 import org.example.assetuijavafx.model.NavigationState;
 import org.example.assetuijavafx.model.TOAssetType;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 
 import static org.example.assetuijavafx.application.AssetPlusApplication.PACKAGE_ID;
 
-public class AssetTypeController implements Initializable {
+public class AssetTypeController extends BaseController implements Initializable {
 
     @FXML
     public BreadCrumbBar<String> breadCrumbBar;
 
-    TreeItem<String> root = new TreeItem<>("AssetType");
     @FXML
     public VBox parentContainer, childContainer;
 
-    private final ArrayList<NavigationState<?>> navigationStack = new ArrayList<>();
+    public AssetTypeController() {
+    }
+
+    @Override
+    protected BreadCrumbBar<String> getBreadcrumbBar() {
+        return breadCrumbBar;
+    }
+
+    @Override
+    protected Pane getChildContainer() {
+        return childContainer;
+    }
+
+    @Override
+    protected Pane getParentContainer() {
+        return parentContainer;
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        initializeBreadcrumbNavigation();
+        super.initializeBreadcrumbNavigation("AssetType");
         parentContainer.addEventHandler(PageSwitchEvent.PAGE_SWITCH, this::changePage);
         parentContainer.fireEvent(new PageSwitchEvent(
                 new NavigationState<>("AssetType",
@@ -36,75 +52,30 @@ public class AssetTypeController implements Initializable {
                         "AssetTypeDisplay.fxml")));
     }
 
-    public void initializeBreadcrumbNavigation() {
-        breadCrumbBar.setOnCrumbAction(event -> {
-            int depth = getCrumbDepth(event.getSelectedCrumb());
-            navigationStack.subList(depth+1, navigationStack.size()).clear();
-            parentContainer.fireEvent(new PageSwitchEvent(navigationStack.get(depth)));
-        });
-    }
-
-    private int getCrumbDepth(TreeItem<String> item) {
-        int depth = 0;
-        while (item.getParent() != null) {
-            depth++;
-            item = item.getParent();
-        }
-        return depth;
-    }
 
 
     public void changePage(PageSwitchEvent event) {
-        if (!childContainer.getChildren().isEmpty()) {
-            childContainer.getChildren().clear();
-        }
-        NavigationState<?> navigationState = event.getNavigationState();
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(PACKAGE_ID.concat(navigationState.getPageName())));
-            Parent child = loader.load();
-            childContainer.getChildren().add(child);
-
-//            if (!navigationStack.isEmpty() && navigationStack.getLast().getFeature().contains("REDIRECT") && navigationState.getFeature().equals("UPDATE"))
-//                navigationState.setFeature("REDIRECT_UPDATE");
-
-            navigationStack.add(navigationState);
-
+            NavigationState<?> navigationState = event.getNavigationState();
+            FXMLLoader loader = super.loadPage(navigationState);
 
             switch (navigationState.getFeature()) {
                 case "DISPLAY":
-                    breadCrumbBar.setSelectedCrumb(root);
+                    super.handleDisplay();
                     break;
                 case "ADD":
-                    TreeItem<String> addItem = new TreeItem<>(navigationState.getTitle());
-                    root.getChildren().add(addItem);
-                    breadCrumbBar.setSelectedCrumb(addItem);
+                    super.handleAdd(navigationState);
                     break;
                 case "UPDATE":
-                    // Handles update for current and other concepts
-                    TreeItem<String> redirectUpdateItem = new TreeItem<>(navigationState.getTitle());
-                    breadCrumbBar.getSelectedCrumb().getChildren().add(redirectUpdateItem);
-                    breadCrumbBar.setSelectedCrumb(redirectUpdateItem);
-                    Object redirectUpdateController = loader.getController();
-                    redirectUpdateController.getClass()
-                            .getMethod("setData", navigationState.getData().getClass())
-                            .invoke(redirectUpdateController, navigationState.getData());
+                    super.handleUpdate(navigationState, loader);
                     break;
                 case "REDIRECT_DISPLAY":
-                    System.out.println("AASASSAS");
-                    System.out.println(breadCrumbBar.getSelectedCrumb());
-                    TreeItem<String> redirectItem = new TreeItem<>(navigationState.getTitle());
-                    breadCrumbBar.getSelectedCrumb().getChildren().add(redirectItem);
-                    breadCrumbBar.setSelectedCrumb(redirectItem);
-                    Object redirectController = loader.getController();
-                    redirectController.getClass()
-                            .getMethod("setData", String.class, Object.class)
-                            .invoke(redirectController, navigationState.getMultiplicity(), navigationState.getData());
+                    super.handleRedirectDisplay(navigationState, loader);
                     break;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
 
     }
 
---
+++ src/main/java/org/example/assetuijavafx/fxml/controllers/BaseController.java
@@ -0,0 +1,87 @@
+package org.example.assetuijavafx.fxml.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.TreeItem;
+import javafx.scene.layout.Pane;
+import org.controlsfx.control.BreadCrumbBar;
+import org.example.assetuijavafx.fxml.utils.PageSwitchEvent;
+import org.example.assetuijavafx.model.NavigationState;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.example.assetuijavafx.application.AssetPlusApplication.PACKAGE_ID;
+
+public abstract class BaseController {
+
+    protected abstract BreadCrumbBar<String> getBreadcrumbBar();
+    protected abstract Pane getParentContainer();
+    protected abstract Pane getChildContainer();
+    protected TreeItem<String> root = new TreeItem<>();
+    protected final ArrayList<NavigationState<?>> navigationStack = new ArrayList<>();
+
+
+    public void initializeBreadcrumbNavigation(String rootPage) {
+        this.root = new TreeItem<>(rootPage);
+        getBreadcrumbBar().setOnCrumbAction(event -> {
+            int depth = getCrumbDepth(event.getSelectedCrumb());
+            navigationStack.subList(depth+1, navigationStack.size()).clear();
+            getParentContainer().fireEvent(new PageSwitchEvent(navigationStack.get(depth)));
+        });
+    }
+
+    protected int getCrumbDepth(TreeItem<String> item) {
+        int depth = 0;
+        while (item.getParent() != null) {
+            depth++;
+            item = item.getParent();
+        }
+        return depth;
+    }
+
+    public FXMLLoader loadPage(NavigationState<?> navigationState) throws IOException {
+        if (!getChildContainer().getChildren().isEmpty()) {
+            getChildContainer().getChildren().clear();
+        }
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(PACKAGE_ID.concat(navigationState.getPageName())));
+        Parent child = loader.load();
+        getChildContainer().getChildren().add(child);
+        navigationStack.add(navigationState);
+        return loader;
+    }
+
+    protected void handleDisplay() {
+        getBreadcrumbBar().setSelectedCrumb(root);
+    }
+
+    protected void handleAdd(NavigationState<?> state) {
+        TreeItem<String> addItem = new TreeItem<>(state.getTitle());
+        root.getChildren().add(addItem);
+        getBreadcrumbBar().setSelectedCrumb(addItem);
+    }
+
+
+    protected void handleRedirectDisplay(NavigationState<?> state, FXMLLoader loader) throws Exception {
+        TreeItem<String> redirectItem = new TreeItem<>(state.getTitle());
+        getBreadcrumbBar().getSelectedCrumb().getChildren().add(redirectItem);
+        getBreadcrumbBar().setSelectedCrumb(redirectItem);
+        Object redirectController = loader.getController();
+        redirectController.getClass()
+                .getMethod("setData", String.class, Object.class)
+                .invoke(redirectController, state.getMultiplicity(), state.getData());
+
+    }
+
+    protected void handleUpdate(NavigationState<?> state, FXMLLoader loader) throws Exception {
+        TreeItem<String> redirectUpdateItem = new TreeItem<>(state.getTitle());
+        getBreadcrumbBar().getSelectedCrumb().getChildren().add(redirectUpdateItem);
+        getBreadcrumbBar().setSelectedCrumb(redirectUpdateItem);
+        Object redirectUpdateController = loader.getController();
+        Class<?> parameterType = state.getData().getClass();
+        redirectUpdateController.getClass()
+                .getMethod("setData", parameterType)
+                .invoke(redirectUpdateController, state.getData());
+    }
+
+}
